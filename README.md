df2fa4d860dffd25b40efa9a4eed457959fd9714
Pascal Bodin <pascal.bodin@systev.com>
2020-06-20 16:37:04 +0200

    test major events and results

--- README.md
+++ README.md
@@ -18,40 +18,54 @@ idp.py -p PORT flash monitor
 
 ```
 ...
-I (0) cpu_start: App cpu up.
-I (452) heap_init: Initializing. RAM available for dynamic allocation:
-I (459) heap_init: At 3FFAFF10 len 000000F0 (0 KiB): DRAM
-I (465) heap_init: At 3FFB6388 len 00001C78 (7 KiB): DRAM
-I (471) heap_init: At 3FFB9A20 len 00004108 (16 KiB): DRAM
-I (477) heap_init: At 3FFBDB5C len 00000004 (0 KiB): DRAM
-I (483) heap_init: At 3FFC88F8 len 00017708 (93 KiB): DRAM
-I (489) heap_init: At 3FFE0440 len 00003AE0 (14 KiB): D/IRAM
-I (496) heap_init: At 3FFE4350 len 0001BCB0 (111 KiB): D/IRAM
-I (502) heap_init: At 40093120 len 0000CEE0 (51 KiB): IRAM
-I (508) cpu_start: Pro cpu start user code
-I (526) spi_flash: detected chip: generic
-I (527) spi_flash: flash io: dio
-I (527) cpu_start: Starting scheduler on PRO CPU.
 I (0) cpu_start: Starting scheduler on APP CPU.
-I (613) BTDM_INIT: BT controller compile version [1ee1530]
-I (613) system_api: Base MAC address is not set, read default base MAC address from BLK0 of EFUSE
-I (713) phy: phy_version: 4180, cb3948e, Sep 12 2019, 16:39:13, 0, 0
-I (1073) RUUVITAGREC: Gap register OK
-I (1083) RUUVITAGREC: Starting scan...
-I (4973) RUUVITAGREC: Frame received:
-I (4973) RUUVITAGREC: 02 01 06 11 ff 99 04 03 72 16 1f bf e6 00 0f 00 
-I (4973) RUUVITAGREC: 06 03 e1 0b 9b 
-I (5963) RUUVITAGREC: Frame received:
-I (5973) RUUVITAGREC: 02 01 06 11 ff 99 04 03 72 16 1f bf e6 00 11 00 
-I (5973) RUUVITAGREC: 06 03 e3 0b a1 
-I (6963) RUUVITAGREC: Frame received:
-I (6973) RUUVITAGREC: 02 01 06 11 ff 99 04 03 72 16 1f bf e6 00 0f 00 
-I (6973) RUUVITAGREC: 06 03 e3 0b 9b 
-I (7963) RUUVITAGREC: Frame received:
-I (7973) RUUVITAGREC: 02 01 06 11 ff 99 04 03 72 16 1f bf e6 00 0e 00 
-I (7973) RUUVITAGREC: 05 03 e2 0b 9b 
-I (8963) RUUVITAGREC: Frame received:
-I (8973) RUUVITAGREC: 02 01 06 11 ff 99 04 03 72 16 1f bf e6 00 0d 00 
-I (8973) RUUVITAGREC: 07 03 e1 0b 9b 
+I (537) RUUVITAGREC: RuuviTagRec 0.2.1
+I (577) BTDM_INIT: BT controller compile version [1ee1530]
+I (577) system_api: Base MAC address is not set, read default base MAC address from BLK0 of EFUSE
+I (687) phy: phy_version: 4180, cb3948e, Sep 12 2019, 16:39:13, 0, 0
+I (1037) RUUVITAGREC: Gap register OK
+I (1047) RUUVITAGREC: Event: SCAN_PARAM_SET_COMPLETE
+I (1047) RUUVITAGREC: Starting scan...
+I (1057) RUUVITAGREC: Event: SCAN_START_COMPLETE
+I (3557) RUUVITAGREC: Event: SCAN_RESULT
+I (3557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (3557) RUUVITAGREC: Frame received:
+I (3567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 08 00 
+I (3567) RUUVITAGREC: 01 03 e3 0b a1 
+I (4557) RUUVITAGREC: Event: SCAN_RESULT
+I (4557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (4557) RUUVITAGREC: Frame received:
+I (4567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 06 00 
+I (4567) RUUVITAGREC: 00 03 e5 0b a7 
+I (5557) RUUVITAGREC: Event: SCAN_RESULT
+I (5557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (5557) RUUVITAGREC: Frame received:
+I (5567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 06 ff 
+I (5567) RUUVITAGREC: ff 03 e4 0b a1 
+I (6557) RUUVITAGREC: Event: SCAN_RESULT
+I (6557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (6557) RUUVITAGREC: Frame received:
+I (6567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 08 00 
+I (6567) RUUVITAGREC: 01 03 e0 0b a1 
+I (7557) RUUVITAGREC: Event: SCAN_RESULT
+I (7557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (7557) RUUVITAGREC: Frame received:
+I (7567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 08 00 
+I (7567) RUUVITAGREC: 02 03 e1 0b a1 
+I (8557) RUUVITAGREC: Event: SCAN_RESULT
+I (8557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (8557) RUUVITAGREC: Frame received:
+I (8567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 07 00 
+I (8567) RUUVITAGREC: 03 03 e1 0b a1 
+I (9557) RUUVITAGREC: Event: SCAN_RESULT
+I (9557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (9557) RUUVITAGREC: Frame received:
+I (9567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 07 00 
+I (9567) RUUVITAGREC: 02 03 e0 0b a7 
+I (10557) RUUVITAGREC: Event: SCAN_RESULT
+I (10557) RUUVITAGREC: Result: SEARCH_INQ_RES
+I (10557) RUUVITAGREC: Frame received:
+I (10567) RUUVITAGREC: 02 01 06 11 ff 99 04 03 64 1a 37 c2 15 00 06 00 
+I (10567) RUUVITAGREC: 03 03 df 0b ad 
 ```
  
--- main/app.c
+++ main/app.c
@@ -31,79 +31,93 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 
-static const char* APP_TAG = "RUUVITAGREC";
+// 1000 ms.
+#define SCAN_INTERVAL 0x0640
+// 500 ms.
+#define SCAN_WINDOW 0x0320
 
-///Declare static functions
+#define APP_VERSION "RuuviTagRec 0.2.1"
+
+static const char APP_TAG[] = "RUUVITAGREC";
+
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
 
 static esp_ble_scan_params_t ble_scan_params = {
     .scan_type              = BLE_SCAN_TYPE_ACTIVE,
     .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
     .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
-    .scan_interval          = 0x50,
-    .scan_window            = 0x30,
+    .scan_interval          = SCAN_INTERVAL,
+    .scan_window            = SCAN_WINDOW,
     .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
 };
+
+static void check_ruuvi_tag_adv(uint8_t *buffer, uint8_t buffer_length) {
+
+}
 
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
     esp_err_t err;
 
     switch (event) {
-    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:{
-        break;
-    }
-    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: {
-        //the unit of the duration is second, 0 means scan permanently
+    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT:
+    	ESP_LOGI(APP_TAG, "Event: SCAN_PARAM_SET_COMPLETE");
+        // The unit of the duration is second, 0 means scan permanently.
         uint32_t duration = 0;
         esp_ble_gap_start_scanning(duration);
         ESP_LOGI(APP_TAG, "Starting scan...");
         break;
-    }
-    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
-        //scan start complete event to indicate scan start successfully or failed
-        if ((err = param->scan_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
-            ESP_LOGE(APP_TAG, "Scan start failed: %s", esp_err_to_name(err));
-        }
-        break;
-    case ESP_GAP_BLE_SCAN_RESULT_EVT: {
-        esp_ble_gap_cb_param_t *scan_result = (esp_ble_gap_cb_param_t *)param;
-        switch (scan_result->scan_rst.search_evt) {
+    case ESP_GAP_BLE_SCAN_RESULT_EVT:
+    	ESP_LOGI(APP_TAG, "Event: SCAN_RESULT");
+        switch (param->scan_rst.search_evt) {
         case ESP_GAP_SEARCH_INQ_RES_EVT:
-//            /* Search for BLE iBeacon Packet */
-//            if (esp_ble_is_ibeacon_packet(scan_result->scan_rst.ble_adv, scan_result->scan_rst.adv_data_len)){
-//                esp_ble_ibeacon_t *ibeacon_data = (esp_ble_ibeacon_t*)(scan_result->scan_rst.ble_adv);
-//                ESP_LOGI(DEMO_TAG, "----------iBeacon Found----------");
-//                esp_log_buffer_hex("IBEACON_DEMO: Device address:", scan_result->scan_rst.bda, ESP_BD_ADDR_LEN );
-//                esp_log_buffer_hex("IBEACON_DEMO: Proximity UUID:", ibeacon_data->ibeacon_vendor.proximity_uuid, ESP_UUID_LEN_128);
-//
-//                uint16_t major = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.major);
-//                uint16_t minor = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.minor);
-//                ESP_LOGI(DEMO_TAG, "Major: 0x%04x (%d)", major, major);
-//                ESP_LOGI(DEMO_TAG, "Minor: 0x%04x (%d)", minor, minor);
-//                ESP_LOGI(DEMO_TAG, "Measured power (RSSI at a 1m distance):%d dbm", ibeacon_data->ibeacon_vendor.measured_power);
-//                ESP_LOGI(DEMO_TAG, "RSSI of packet:%d dbm", scan_result->scan_rst.rssi);
-//            }
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_INQ_RES");
             ESP_LOGI(APP_TAG, "Frame received:");
-            ESP_LOG_BUFFER_HEX(APP_TAG, scan_result->scan_rst.ble_adv, scan_result->scan_rst.adv_data_len);
+            ESP_LOG_BUFFER_HEX(APP_TAG, param->scan_rst.ble_adv, param->scan_rst.adv_data_len);
             // Frame example: 02 01 06 11 ff 99 04 03 6f 17 11 bf 51 00 0c 00 04 03 dd 0b a1
-            break;
+        	break;
+        case ESP_GAP_SEARCH_INQ_CMPL_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_INQ_CMPL");
+        	break;
+        case ESP_GAP_SEARCH_DISC_RES_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_DISC_RES");
+        	break;
+        case ESP_GAP_SEARCH_DISC_BLE_RES_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_DISC_BLE_RES");
+        	break;
+        case ESP_GAP_SEARCH_DISC_CMPL_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_DISC_CMPL");
+        	break;
+        case ESP_GAP_SEARCH_DI_DISC_CMPL_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_DI_DISC_CMPL");
+        	break;
+        case ESP_GAP_SEARCH_SEARCH_CANCEL_CMPL_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_CANCEL_CMPL");
+        	break;
+        case ESP_GAP_SEARCH_INQ_DISCARD_NUM_EVT:
+        	ESP_LOGI(APP_TAG, "Result: SEARCH_INQ_DISCARD_NUM");
+        	break;
         default:
-            break;
+        	ESP_LOGI(APP_TAG, "Result: unknown value - %d", param->scan_rst.search_evt);
+        	break;
         }
         break;
-    }
-
+    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
+    	ESP_LOGI(APP_TAG, "Event: SCAN_START_COMPLETE");
+    	err = param->scan_start_cmpl.status;
+    	if (err != ESP_BT_STATUS_SUCCESS) {
+    		ESP_LOGE(APP_TAG, "Scan start failed: %s", esp_err_to_name(err));
+    	}
+    	break;
     case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
-        if ((err = param->scan_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
+    	ESP_LOGI(APP_TAG, "Event: SCAN_STOP_COMPLETE");
+    	err = param->scan_stop_cmpl.status;
+        if (err != ESP_BT_STATUS_SUCCESS) {
             ESP_LOGE(APP_TAG, "Scan stop failed: %s", esp_err_to_name(err));
         }
-        else {
-            ESP_LOGI(APP_TAG, "Stop scan successfully");
-        }
         break;
-
     default:
+    	ESP_LOGI(APP_TAG, "Event: %d", event);
         break;
     }
 }
@@ -113,6 +127,8 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
  */
 void app_main(void)
 {
+	ESP_LOGI(APP_TAG, APP_VERSION);
+
     esp_err_t status;
 
     ESP_ERROR_CHECK(nvs_flash_init());
